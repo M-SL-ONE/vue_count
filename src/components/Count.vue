6d7f84bf7fe5ed476be4ce2ddf727e244f0e61cb
leah <sljinyao@qq.com>
2025-07-23 15:00:33 +0800

    mapMutations和mapActions优化代码

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -7,15 +7,15 @@
       <option value="2">2</option>
       <option value="3">3</option>
     </select>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
-    <button @click="incrementOdd">当前求和为奇数再加</button>
-    <button @click="incrementWait">等一等再加</button>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <button @click="incrementOdd(n)">当前求和为奇数再加</button>
+    <button @click="incrementWait(n)">等一等再加</button>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
+import { mapGetters, mapState, mapMutations ,mapActions } from 'vuex';
 export default {
   name: "CountItem",
   data() {
@@ -38,7 +38,7 @@ export default {
     // bigSum(){
     //     return this.$store.getters.bigSum
     // }
-    
+
     // 借助mapState生成计算属性，从state中读取数据（对象写法））
     ...mapGetters({bigSum:'bigSum'}),
 
@@ -46,31 +46,41 @@ export default {
     // ...mapGetters(['bigSum'])
   },
   methods: {
-    increment() {
-      //   this.sum += this.n;
-    //   this.$store.dispatch("add", this.n);
-      this.$store.commit("ADD", this.n);
-    },
-    decrement() {
-      //   this.sum -= this.n;
-    //   this.$store.dispatch("jian", this.n);
-      this.$store.commit("JIAN", this.n);
-    },
-    incrementOdd() {
-      // if(this.sum%2!=0) return alert('当前不是奇数')
-      // this.sum += this.n;
-    //   if (this.$store.state.sum % 2) 
-      this.$store.dispatch("addOdd", this.n);
-    },
-    incrementWait() {
-      // setTimeout(() => {
-      //     this.sum += this.n;
-      // }, 500);
-      this.$store.dispatch("addWait", this.n);
-    //   setTimeout(() => {
+    // increment() {
+    //   //   this.sum += this.n;
+    // //   this.$store.dispatch("add", this.n);
+    //   this.$store.commit("ADD", this.n);
+    // },
+    // decrement() {
+    //   //   this.sum -= this.n;
+    // //   this.$store.dispatch("jian", this.n);
+    //   this.$store.commit("JIAN", this.n);
+    // },
+
+    // 借助mapMutations生成计算属性，从state中读取数据（对象写法））
+    ...mapMutations({increment:`ADD`,decrement:"JIAN"}),
+
+    /**
+     * 
+     */
+    // incrementOdd() {
+    //   // if(this.sum%2!=0) return alert('当前不是奇数')
+    //   // this.sum += this.n;
+    // //   if (this.$store.state.sum % 2) 
+    //   this.$store.dispatch("addOdd", this.n);
+    // },
+    // incrementWait() {
+    //   // setTimeout(() => {
+    //   //     this.sum += this.n;
+    //   // }, 500);
+    //   this.$store.dispatch("addWait", this.n);
+    // //   setTimeout(() => {
         
-    //   }, 500);
-    },
+    // //   }, 500);
+    // },
+    
+    // 借助mapActions生成计算属性，从state中读取数据（对象写法））
+    ...mapActions({incrementOdd:'addOdd',incrementWait:'addWait'})
   },
   mounted() {
     // console.log(this)
