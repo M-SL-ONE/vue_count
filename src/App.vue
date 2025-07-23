f5a470dd27223628f3792bd1b6f805b7a9ddc539
leah <sljinyao@qq.com>
2025-07-23 09:12:03 +0800

    vue实现求和

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <CountItem/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountItem from './components/Count.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountItem
   }
 }
 </script>
---
+++ src/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ sum }}</h1>
+    <select name="" id="" v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CountItem",
+  data() {
+    return {
+      sum: 0, //当前的和
+      n: 1, // 用户选择的数字
+    };
+  },
+  methods: {
+    increment() {
+      this.sum += this.n;
+    },
+    decrement() {
+      this.sum -= this.n;
+    },
+    incrementOdd() {
+        if(this.sum%2!=0) return alert('当前不是奇数')
+        this.sum += this.n;
+    },
+    incrementWait() {
+        setTimeout(() => {
+            this.sum += this.n;
+        }, 500);
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+button {
+  margin-left: 5px;
+}
+</style>
